f43bee43f40f1565ad46151bac4effbec17a3de8
herlonchagas <herlonchagas.c@gmail.com>
2018-06-22 19:04:12 -0300

    continuação da listagem do cardapio

--- src/pages/cardapios/cardapios.ts
+++ src/pages/cardapios/cardapios.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
-import {NavController, NavParams} from "ionic-angular";
+import {AlertController, LoadingController, NavController, NavParams} from "ionic-angular";
 import { Http } from "@angular/http";
 import {Restaurante} from "../../domain/restaurante/restaurante";
+import { Cardapio} from "../../domain/cardapio/cardapio";
 
 @Component({
   selector: 'page-cardapios',
@@ -11,13 +12,44 @@ import {Restaurante} from "../../domain/restaurante/restaurante";
 export class CardapiosPage {
 
   public restaurante: Restaurante;
+  public url: string;
+  private cardapios: Cardapio;
   constructor(
     public navCtrl: NavController,
     private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
     public navParams: NavParams
   ){
     this.restaurante = this.navParams.get('RestauranteSelecionado');
     console.log(this.restaurante.nome);
+    this.url = "http://marmita.idsgeo.com/index.php/page/get_ionic_cardapio_json/"+this.restaurante.id;
   }
+  ngOnInit(){
+    let loader = this._loadingCtrl.create({
+      content: "Buscando cardapio. Aguarde..."
+    });
+    loader.present();
+    this._http
+      .get(this.url)
+      .map( res => res.json())
+      .toPromise()
+      .then( cardapios => {
+        this.cardapios = cardapios;
+        loader.dismiss();
+      })
+      .catch(err =>{
+        console.log(err);
+        loader.dismiss();
+        this._alertCtrl
+          .create({
+            title: "Falha na conexão",
+            buttons: [{text: "Ok, estou ciente!"}],
+            subTitle: "Não foi possivel obter o cardapio. Tente mais tarde."
+          }).present();
+      });
+  }
+  submit(cardapio){
 
+  }
 }
